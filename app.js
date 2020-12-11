852d005dbfe5a083187c4b069f8a25d51846439c
ccurnuck <calvin.curnuck@gmail.com>
2020-12-11 13:48:07 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => console.log('HI'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
